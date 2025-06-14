45828e900703c4b8de1996312e8810a4aeaccbf2
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-06-15 00:45:54 +0530

    product demo-data

---
+++ src/lib/demoData.ts
@@ -0,0 +1,15 @@
+export const demoData = [
+  { id: "0", product_name: "T-Shart", product_imag: "", product_price: 299 },
+
+  { id: "1", product_name: "Shart", product_imag: "", product_price: 499 },
+
+  { id: "2", product_name: "SkayBag", product_imag: "", product_price: 999 },
+
+  {
+    id: "3",
+    product_name: "IQOO Neo-10R",
+    product_imag: "",
+    product_price: 33999,
+  },
+  { id: "4", product_name: "IQOO 12", product_imag: "", product_price: 45999 },
+];
