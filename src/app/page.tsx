9a31003812c4bc0dae107dd0c184eae39c7b7b32
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-06-17 15:56:18 +0530

    Items addto cart
    user can easily added items add to cart. An item will only be added once.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import CardItemDisplay from "@/components/CardItemDisplay";
+import { demoData } from "@/lib/demoData";
 
 const page = () => {
   return (
     <>
-      <div className="grid place-items-center gap-5 pt-20 sm:grid-cols-4">
-        <CardItemDisplay />
-        <CardItemDisplay />
-        <CardItemDisplay />
-        <CardItemDisplay />
-        <CardItemDisplay />
-        <CardItemDisplay />
+      <div className="grid place-items-center gap-5 pt-20 sm:grid-cols-3">
+        {demoData.map((items) => (
+          <CardItemDisplay cardInfo={items} key={items.id} />
+        ))}
       </div>
     </>
   );
---
+++ src/lib/atom.ts
@@ -0,0 +1,4 @@
+import { atom } from "jotai";
+import { DataType } from "./type";
+
+export const cartAtom = atom<DataType[]>([]);
